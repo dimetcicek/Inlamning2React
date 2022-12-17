4c857ada7867f4264ec7cb88eb1354a24f71ff14
Dimet Cicek <dimet.cicek96@hotmail.com>
2022-12-17 13:39:37 +0100

    Cleaned up errors and added failed submit

--- react-webapp/src/sections/ContactFormSection.js
+++ react-webapp/src/sections/ContactFormSection.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 
 const ContactFormSection = () => {
-    const [contactForm, setContactForm] = useState({name: '', email: '', comments: ''})
-    const [nameError, setNameError] = useState('')
-    const [emailError, setEmailError] = useState('')
-    const [commentsError, setCommentsError] = useState('')
+    const [contactForm, setContactForm] = useState({ name: '', email: '', comments: '' })
+    const [errors, setErrors] = useState({ nameError: '', emailError: '', commentsErrror: '' })
     const [submitted, setSubmitted] = useState(false)
+    const [failedSubmit, setFailedSubmit] = useState(false)
 
     const validateName = (name) => {
-        var error
+        var error = null
+
         if(!name)
             error = "You must enter a name"
 
@@ -16,7 +16,7 @@ const ContactFormSection = () => {
     }
 
     const validateEmail = (email) => {
-        var error
+        var error = null
         const regex_email = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
         if(!email)
@@ -28,7 +28,7 @@ const ContactFormSection = () => {
     }
 
     const validateComments = (comments) => {
-        var error
+        var error = null
 
         if(!comments)
             error = "You must enter a comment"
@@ -40,89 +40,86 @@ const ContactFormSection = () => {
 
     const handleNameChange = (e) => {
         const {id, value} = e.target
-        setContactForm({...contactForm, [id]: value})
-        setNameError(validateName(value))
+        setContactForm({ ...contactForm, [id]: value })
+        setErrors({ ...errors, nameError: validateName(value)})
     }
 
     const handleEmailChange = (e) => {
         const {id, value} = e.target
-        setContactForm({...contactForm, [id]: value})
-        setEmailError(validateEmail(value))
+        setContactForm({ ...contactForm, [id]: value })
+        setErrors({ ...errors, emailError: validateEmail(value) })
     }
 
     const handleCommentsChange = (e) => {
         const {id, value} = e.target
-        setContactForm({...contactForm, [id]: value})
-        setCommentsError(validateComments(value))
+        setContactForm({ ...contactForm, [id]: value })
+        setErrors({ ...errors, commentsError: validateComments(value) })
     }
 
     const handleSubmit = (e) =>
     {
         e.preventDefault()
-        const errors = {}
+
         let name = contactForm.name
         let email = contactForm.email
         let comments = contactForm.comments
 
-        var nameError = validateName(name)
-        var emailError = validateEmail(email)
-        var commentsError = validateComments(comments)
+        let nameError = validateName(name)
+        let emailError = validateEmail(email)
+        let commentsError = validateComments(comments)
 
-        if(nameError) {
-            errors.name = nameError
-            setNameError(errors.name)
+        setErrors({ nameError: nameError, emailError: emailError, commentsError: commentsError })
+
+        if (nameError === null && emailError === null && commentsError === null)
+        {
+            var jsonData = JSON.stringify({ name, email, comments })
+        
+            setContactForm({ name: '', email: '', comments: '' })
+            setErrors({ nameError: '', emailError: '', commentsError: '' })
+        
+            fetch("https://win22-webapi.azurewebsites.net/api/contactform",
+                {
+                    method: "POST",
+                    headers:
+                    {
+                        "Content-Type": "application/json"
+                    },
+                    body: jsonData
+                })
+            .then(result => {
+                if (result.status === 200) {
+                    setSubmitted(true)
+                    setFailedSubmit(false)
+                }
+                else {
+                    setSubmitted(false)
+                    setFailedSubmit(true)
+                }
+            })
         }
-
-        if(emailError) {
-            errors.email = emailError
-            setEmailError(errors.email)
+        else
+        {
+            setSubmitted(false)
         }
-
-        if(commentsError) {
-            errors.comments = commentsError
-            setCommentsError(errors.comments)
-        }
-
-       if (Object.keys(errors).length === 0)
-       {
-           var jsonData = JSON.stringify({ name, email, comments })
-
-           setContactForm({ name: '', email: '', comments: '' })
-           setNameError('')
-           setEmailError('')
-           setCommentsError('')
-
-           fetch("https://win22-webapi.azurewebsites.net/api/contactform",
-               {
-                   method: "POST",
-                   headers:
-                   {
-                       "Content-Type": "application/json"
-                   },
-                   body: jsonData
-               })
-           .then(results => {
-               if (results.status === 200) {
-                   setSubmitted(true)
-               }
-               else {
-                   setSubmitted(false)
-               }
-           })
-       }
-       else
-       {
-           setSubmitted(false)
-       }
     }
-  
-
 
     return (
         <section className="contact-form">
             <div className="container">
                 {
-                    submitted ? 
+                    failedSubmit ?
+                    (
+                        <div>
+                            <div className="alert alert-danger text-center">
+                                <h1>Failed to submit!</h1>
+                            </div>
+                        </div>)
+                        :
+                        (<></>)
+                }
+
+                {
+                    submitted ?
                     (<div>
                         <div className ="submit-message">
                             <h1>Thank you for your comment!</h1>
@@ -135,15 +132,15 @@ const ContactFormSection = () => {
                         <form onSubmit={handleSubmit} noValidate>
                             <div>
                                 <input id="name" type="text" placeholder="Your Name" value={contactForm.name} onChange={handleNameChange} />
-                                <div className="errorMessage">{nameError}</div>
+                                <div className="errorMessage">{errors.nameError}</div>
                             </div>
                             <div>
                                 <input id="email" type="email" placeholder="Your Mail" value={contactForm.email} onChange={handleEmailChange}/>
-                                <div className="errorMessage">{emailError}</div>
+                                <div className="errorMessage">{errors.emailError}</div>
                             </div>
                             <div className ="textarea">
                                 <textarea id="comments" placeholder="Comments" value={contactForm.comments} onChange={handleCommentsChange}></textarea>
-                                <div className="errorMessage">{commentsError}</div>
+                                <div className="errorMessage">{errors.commentsError}</div>
                             </div>
                             <div className="formBtn">
                                 <button type="submit" className="btn-theme">Post Comments</button>
